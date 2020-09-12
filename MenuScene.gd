3708f06a5feed238c08ead014c22c4402da8acb6
oaparfene <oaparfene@gmail.com>
2020-09-12 20:47:55 +0200

    misc

---
+++ MenuScene.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_labPlay_pressed():
+	
+	get_tree().change_scene("res://Runway.tscn")
+	pass # Replace with function body.
