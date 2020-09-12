87c92011e786ea5250aaa0ffc69eaedc4fd7b800
oaparfene <oaparfene@gmail.com>
2020-09-12 21:44:58 +0200

    garage

--- MenuScene.gd
+++ MenuScene.gd
@@ -6,6 +6,7 @@ func _ready():
 
 
 func _on_labPlay_pressed():
-	
 	get_tree().change_scene("res://Runway.tscn")
-	pass # Replace with function body.
+
+func _on_labGarage_pressed():
+	get_tree().change_scene("res://Garage.tscn")
