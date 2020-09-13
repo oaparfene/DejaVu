e8de41f155305db8ed401695a4309a9691b4f3c2
Daniel Jorden <12djorden@gmail.com>
2020-09-13 18:24:50 +0100

    added fuel

--- bodyCar.gd
+++ bodyCar.gd
@@ -5,14 +5,23 @@ onready var partTyre = $partTyre
 onready var timerAttack = $timerAttack
 
 var bodyBullet_load = preload("res://bodyBullet.tscn")
+var velocity = Vector2.ZERO
 
 func _ready():
 	timerAttack.wait_time = 0.1
 
 func _physics_process(delta):
 	partDirt.initial_velocity = Globals.parSpeed*delta*17
-	move_and_collide(Globals.carVector*delta*Globals.carSpeed)
+	if Globals.carFuel["current"] > 0:
+		velocity = Globals.carVector*delta*Globals.carSpeed
+	else:
+		velocity = Vector2(0,Globals.parSpeed)*delta
+	var kinCollisionInfo = move_and_collide(velocity)
+	if kinCollisionInfo: # If we collided
+		if "Lose" in kinCollisionInfo.collider.name:
+			get_tree().change_scene("res://Garage.tscn")
 	partTyre.emitting = Globals.carVector.y > 50
+	Globals.changeFuel(-16*delta)
 
 
 func _on_timerAttack_timeout():
