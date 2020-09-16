5a9c963c1a35d7cf7ad4a01d7f100965deca8671
Daniel Jorden <12djorden@gmail.com>
2020-09-16 20:21:28 +0100

    changed physics

--- Scripts/classCar.gd
+++ Scripts/classCar.gd
@@ -19,7 +19,7 @@ var armor:float
 var money
 
 func damage(dmg):
-	var damage = ( (1-armor/100.0)*dmg ) / 5
+	var damage = ( (1-armor/100.0)*dmg ) / 200
 	health -= damage
 	if team == "enemy":
 		if health <= 0:
@@ -32,15 +32,24 @@ func damage(dmg):
 	$prgHealth.value = 100*(health/maxHealth)
 	#print(name," took ",damage," dmg")
 
-func carCollision(info:KinematicCollision2D):
+func carCollision(info:KinematicCollision2D,delta:float):
 	var enemy = info.collider
-	var impactSpeed = abs(enemy.velocity.dot(info.normal)) # Calculate impact
-	var impactImpulse = impactSpeed * enemy.mass
-	var newImpulse = velocity.dot(info.normal) - impactImpulse
+	var colliderImpulse = -enemy.velocity.dot(info.normal) * enemy.mass # Calculate impact
+	var impulse = -velocity.dot(info.normal) * mass
+	var newImpulse = impulse + colliderImpulse
 	var newSpeed = newImpulse/mass
-	actVector *= newSpeed/speed
+	print(
+		team,"(",mass,") ",
+		"S:",stepify(-velocity.dot(info.normal),0.1)," I:",stepify(impulse,0.1),
+		" -> ",
+		"I:",stepify(colliderImpulse,0.1)," S:",stepify(-enemy.velocity.dot(info.normal),0.1),
+		" (",enemy.mass,")"+enemy.team
+	)
+	print(team,": [ nS:",newSpeed," nI:",newImpulse,"]")
+	print(team," Before: ",actVector)
+	actVector = actVector.normalized()*clamp(newSpeed/(speed*delta),-1,1)
+	print(team," After: ",actVector)
 	#print(name," took ",impactSpeed," impact")
-	if impactSpeed > 5: # Ignore small collisions
-		var damage = impactSpeed * enemy.mass
-		damage(damage)
+	if impulse > 0: # Ignore small collisions
+		enemy.damage(impulse)
 	#actVector = actVector.bounce(info.normal)*(enemy.mass/(enemy.mass+mass))
