50a22ae2f0f34c7e053b0dcb7da916bc59417f39
oaparfene <oaparfene@gmail.com>
2020-09-16 18:23:19 +0200

    misc

--- Scripts/weaponSlot.gd
+++ Scripts/weaponSlot.gd
@@ -2,6 +2,8 @@ extends Button
 
 var popup
 
+var load_objGridItem = preload("res://Scenes/objGridItem.tscn")
+
 func _ready():
 	$labDescription.text = "Weapons Slot 1"
 
@@ -12,7 +14,18 @@ func _on_weaponSlot_pressed():
 	popup.connect("popup_hide", self, "deletePopups")
 	var gridCtn = GridContainer.new()
 	popup.add_child(gridCtn)
-	#for gunIndex in Globals.upgs[]
+	for gunIndex in Globals.gunNameArray:
+		if Globals.upgs[gunIndex]["unlocked"] == true:
+			var objGridItem = load_objGridItem.instance()
+			objGridItem.currentWeaponName = gunIndex
+			objGridItem.get_node("sprWeapon").texture = load("res://Assets/Guns/img_" + gunIndex + ".png")
+			objGridItem.connect("weapon_selected", self, "setWeaponSlot")
+			gridCtn.add_child(objGridItem)
+	add_child(popup)
+	popup.popup_centered()
 
 func deletePopups():
 	popup.queue_free()
+
+func setWeaponSLot(name):
+	pass
