e42df87685a900631d5c452b3d49952793fd8ee6
Daniel Jorden <12djorden@gmail.com>
2020-09-23 04:20:18 +0100

    added random bgs for main menu, fixed rocket bug

--- Scripts/texBackground.gd
+++ Scripts/texBackground.gd
@@ -1,5 +1,11 @@
 extends TextureRect
 
+var bgs = ["desert","forest","snow","night"]
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	rng.randomize()
+	texture = load("res://Assets/Levels/img_"+bgs[rng.randi_range(0,3)]+".png")
 
 func _physics_process(delta):
 	
--- Scripts/texLogo.gd
+++ Scripts/texLogo.gd
@@ -3,7 +3,7 @@ extends TextureRect
 onready var timer = $timer
 
 func _physics_process(delta):
-	rect_position.y = sin(2*PI*timer.time_left)*1000*delta
+	rect_position.y = 50 + sin(2*PI*timer.time_left)*1000*delta
 
 func _on_timer_timeout():
 	timer.start()
