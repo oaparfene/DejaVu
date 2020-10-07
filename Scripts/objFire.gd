049f73edf09ae8f30ed68a682b89831ebb619cb7
Daniel Jorden <12djorden@gmail.com>
2020-10-07 16:06:44 +0100

    bug fixes

--- Scripts/objFire.gd
+++ Scripts/objFire.gd
@@ -1,4 +1,4 @@
-extends TouchScreenButton
+extends Button
 
 var gunName
 
@@ -7,21 +7,20 @@ export(int) var slot
 func _ready():
 	gunName = Globals.getGunInSlotName(slot)
 	if gunName != "empty":
-		get_node("../sprWeapon").texture = load("res://Assets/Guns/img_"+gunName+".png")
+		$sprWeapon.texture = load("res://Assets/Guns/img_"+gunName+".png")
 	else:
-		get_parent().visible = false
+		visible = false
 
 func updateUI(slots):
 	if gunName == "empty":
 		return
-	modulate = Color(1,1,1) * (1-slots[gunName]["timer"].time_left/slots[gunName]["timer"].wait_time)
-	get_node("../labAmmo").text = str(slots[gunName]["ammo"])
+	$sprWeapon.modulate = Color(1,1,1) * (1-slots[gunName]["timer"].time_left/slots[gunName]["timer"].wait_time)
+	$labAmmo.text = str(slots[gunName]["ammo"])
 
-func _on_btnFire_pressed():
+func _on_btnFire_mouse_entered():
 	get_tree().call_group("player","fireGun",Globals.getGunInSlotName(slot))
-	get_parent().modulate = Color(1,0,0)
+	modulate = Color(1,0,0)
 
-
-func _on_btnFire_released():
+func _on_btnFire_mouse_exited():
 	get_tree().call_group("player","ceaseGun",Globals.getGunInSlotName(slot))
-	get_parent().modulate = Color(1,1,1)
+	modulate = Color(1,1,1)
