c946bd1232eee07ccf0070f1618d8d14f371bec4
oaparfene <oaparfene@gmail.com>
2020-10-11 18:13:28 +0200

    fixed gun UI bug

--- Scripts/GunShop.gd
+++ Scripts/GunShop.gd
@@ -9,13 +9,14 @@ func updateUI():
 	$btnLeft.visible = not (Globals.getGunIndex() == 0)
 	$btnRight.disabled = (Globals.getGunIndex() == Globals.getNoOfGuns() - 1)
 	$btnRight.visible = not (Globals.getGunIndex() == Globals.getNoOfGuns() - 1)
-	$texGun.texture = load("res://Assets/Guns/img_"+str(Globals.getGunName())+".png")
+	$texGun.texture = load("res://Assets/Guns/img_"+ $TabContainer/Skins.skinName +".png")
 	$texGun.self_modulate = Color(0.1,0.1,0.1)+Color(0.9,0.9,0.9)*int(Globals.getUnlocked(Globals.getGunName()))
 	$texGun/btnUnlock.visible = not Globals.getUnlocked(Globals.getGunName())
 	$texGun/btnUnlock.text = str(Globals.getUnlockCostGun()) + "$"
 	get_tree().call_group("upgrade","updateUI")
 	get_tree().call_group("moneyUI","updateUI")
 	get_tree().call_group("wpnSlotsUI","updateUI")
+	get_tree().call_group("skinUI","updateUI")
 
 func _on_btnRight_pressed():
 	Globals.nextGun()
